64f4a5d1a20a86e4b0ad52c10fbe8fafbd95b519
Mike Barkas <mike@barkas.com>
2014-11-30 22:36:42 -0500

    Weather api call and html template.

--- app.js
+++ app.js
@@ -41,9 +41,21 @@ wxApp.controller('homeController', ['$scope', 'cityService',
 
 }]);
 
-wxApp.controller('forecastController', ['$scope', 'cityService',
-  function($scope, cityService) {
+wxApp.controller('forecastController', ['$scope', '$resource', 'cityService',
+  function($scope, $resource, cityService) {
 
     $scope.city = cityService.city;
+
+    $scope.weatherAPI =
+      $resource('http://api.openweathermap.org/data/2.5/forecast/daily', {
+        callback: 'JSON_CALLBACK'}, { get: {method: 'JSONP'}
+      });
+
+    $scope.weatherResult = $scope.weatherAPI.get({
+      q: $scope.city,
+      cnt: 2
+    });
+
+    console.log($scope.weatherResult);
 
 }]);
