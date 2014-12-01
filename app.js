c86ac884d001bc50dc86706d33195fa5618dc61d
Mike Barkas <mike@barkas.com>
2014-11-30 21:52:13 -0500

    Basic router and place holder text.

--- app.js
+++ app.js
@@ -1,1 +1,33 @@
+/*
+ * Angular module.
+ */
 var wxApp = angular.module('wxApp', ['ngRoute', 'ngResource']);
+
+/*
+ * Routes.
+ */
+wxApp.config(function($routeProvider) {
+
+  $routeProvider
+    .when('/', {
+      templateUrl: 'pages/home.html',
+      controller: 'homeController'
+    })
+    .when('/forecast', {
+      templateUrl: 'pages/forecast.html',
+      controller: 'forecastController'
+    })
+});
+
+/*
+ * Controllers.
+ */
+wxApp.controller('homeController', ['$scope', function($scope) {
+
+
+}]);
+
+wxApp.controller('forecastController', ['$scope', function($scope) {
+
+
+}]);
